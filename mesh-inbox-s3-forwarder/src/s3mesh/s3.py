204b519e7b9a0704924a362b732198387f2fcbde
Helen Zhou <helen.zhou1@nhs.net>
2020-11-20 17:59:25 +0000

    PMRT-1349 [JG/HZ] Implements S3Uploader

--- mesh-inbox-s3-forwarder/src/s3mesh/s3.py
+++ mesh-inbox-s3-forwarder/src/s3mesh/s3.py
@@ -2,5 +2,10 @@ from s3mesh.mesh import MeshMessage
 
 
 class S3Uploader:
+    def __init__(self, s3_client, bucket_name: str):
+        self._s3_client = s3_client
+        self._bucket_name = bucket_name
+
     def upload(self, message: MeshMessage):
-        pass
+        key = f"{message.date_delivered.strftime('%Y/%m/%d')}/{message.file_name}"
+        self._s3_client.upload_fileobj(message, self._bucket_name, key)
---
+++ mesh-inbox-s3-forwarder/tests/unit/test_s3.py
@@ -0,0 +1,20 @@
+from unittest.mock import MagicMock
+
+from s3mesh.s3 import S3Uploader
+from datetime import datetime
+
+
+def test_upload():
+    mock_s3_client = MagicMock()
+    bucket_name = "test_bucket"
+    mesh_message = MagicMock()
+    file_name = "a_file_A1BH13.dat"
+    mesh_message.file_name = file_name
+    mesh_message.date_delivered = datetime(year=2020, month=11, day=2)
+
+    uploader = S3Uploader(mock_s3_client, bucket_name)
+    uploader.upload(mesh_message)
+
+    mock_s3_client.upload_fileobj.assert_called_once_with(
+        mesh_message, bucket_name, f"2020/11/02/{file_name}"
+    )
