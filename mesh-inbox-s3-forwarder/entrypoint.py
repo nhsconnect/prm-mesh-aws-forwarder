79aed8dd84cf50eb42c77ee14e3382fe6db81f9a
James Green <jagreen@thoughtworks.com>
2020-12-01 15:26:01 +0000

    PMRT-1349 [DS/JG] Read password and shared key from parameter store

--- mesh-inbox-s3-forwarder/entrypoint.py
+++ mesh-inbox-s3-forwarder/entrypoint.py
@@ -22,8 +22,8 @@ def read_env(env_var):
 class Config:
     mesh_url: str
     mesh_mailbox: str
-    mesh_password: str
-    mesh_shared_key: str
+    mesh_password_ssm_param: str
+    mesh_shared_key_ssm_param: str
     mesh_client_cert_ssm_param: str
     mesh_client_key_ssm_param: str
     mesh_ca_cert_ssm_param: str
@@ -41,11 +41,14 @@ class Config:
         )
 
 
-def download_ssm_param(ssm, param_name, file_path):
+def read_ssm_param(ssm, param_name):
     response = ssm.get_parameter(Name=param_name, WithDecryption=True)
-    parameter = response['Parameter']['Value']
+    return response['Parameter']['Value']
+
+
+def write_file(contents, file_path):
     with open(file_path, 'w') as f:
-        f.write(parameter)
+        f.write(contents)
 
 
 def main():
@@ -57,9 +60,16 @@ def main():
     mesh_client_key_path = join(config.forwarder_home, "client_key.pem")
     mesh_ca_cert_path = join(config.forwarder_home, "ca_cert.pem")
 
-    download_ssm_param(ssm, config.mesh_client_cert_ssm_param, mesh_client_cert_path)
-    download_ssm_param(ssm, config.mesh_client_key_ssm_param, mesh_client_key_path)
-    download_ssm_param(ssm, config.mesh_ca_cert_ssm_param, mesh_ca_cert_path)
+    mesh_client_cert = read_ssm_param(ssm, config.mesh_client_cert_ssm_param)
+    mesh_client_key = read_ssm_param(ssm, config.mesh_client_key_ssm_param)
+    mesh_ca_cert = read_ssm_param(ssm, config.mesh_ca_cert_ssm_param)
+
+    mesh_password = read_ssm_param(ssm, config.mesh_password_ssm_param)
+    mesh_shared_key = read_ssm_param(ssm, config.mesh_shared_key_ssm_param)
+
+    write_file(mesh_client_cert, mesh_client_cert_path)
+    write_file(mesh_client_key, mesh_client_key_path)
+    write_file(mesh_ca_cert, mesh_ca_cert_path)
 
     logging.basicConfig(stream=stdout, level=logging.INFO, format=LOG_FORMAT)
 
@@ -67,8 +77,8 @@ def main():
         mesh_config=MeshConfig(
             url=config.mesh_url,
             mailbox=config.mesh_mailbox,
-            password=config.mesh_password,
-            shared_key=bytes(config.mesh_shared_key, "utf-8"),
+            password=mesh_password,
+            shared_key=bytes(mesh_shared_key, "utf-8"),
             client_cert_path=mesh_client_cert_path,
             client_key_path=mesh_client_key_path,
             ca_cert_path=mesh_ca_cert_path,
