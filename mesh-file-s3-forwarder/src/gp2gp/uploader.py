e8ecafe129e71c6f1ba6d89a53a8b20588b73f68
James Green <jagreen@thoughtworks.com>
2020-12-02 16:03:17 +0000

    PMRT-1349 [DS/JG] Remove redundant mesh file forwarder

--- mesh-file-s3-forwarder/src/gp2gp/uploader.py
+++
@@ -1,9 +0,0 @@
-class MeshS3Uploader:
-    def __init__(self, s3, bucket_name):
-        self._s3 = s3
-        self._bucket_name = bucket_name
-
-    def upload(self, mesh_file):
-        date_delivered = mesh_file.read_delivery_date()
-        key = f"{date_delivered.strftime('%Y/%m/%d')}/{mesh_file.path.name}"
-        self._s3.upload_file(str(mesh_file.path), self._bucket_name, key)
