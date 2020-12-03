7693a96950da777f0804e7bd1b74c956d1ba89e0
Dorota-TW <dorota.sobkow1@nhs.net>
2020-12-03 11:24:12 +0000

    PMRT-1349 [JG/DS] Download secret to a file

--- src/s3mesh/secrets.py
+++ src/s3mesh/secrets.py
@@ -5,3 +5,8 @@ class SsmSecretManager:
     def get_secret(self, name):
         response = self._ssm.get_parameter(Name=name, WithDecryption=True)
         return response["Parameter"]["Value"]
+
+    def download_secret(self, name, output_file_path):
+        secret = self.get_secret(name)
+        with open(output_file_path, "w") as f:
+            f.write(secret)
--- tests/unit/test_secret_manager.py
+++ tests/unit/test_secret_manager.py
@@ -3,6 +3,11 @@ from mock import MagicMock
 from s3mesh.secrets import SsmSecretManager
 
 
+def _read_file(file_path):
+    with open(file_path) as f:
+        return f.read()
+
+
 def _build_mock_ssm_client(secrets):
     def mock_get_param(**kwargs):
         return {"Parameter": {"Value": secrets[kwargs["Name"]]}}
@@ -20,6 +25,22 @@ def test_secret_manger_reads_from_ssm():
     secret_manager = SsmSecretManager(mock_ssm_client)
 
     actual_secret_value = secret_manager.get_secret(secret_name)
+
+    assert actual_secret_value == secret_value
+    mock_ssm_client.get_parameter.assert_called_once_with(Name=secret_name, WithDecryption=True)
+
+
+def test_secret_manger_downloads_to_a_file(fs):
+    secret_name = "fruit"
+    secret_value = "mango"
+    mock_ssm_client = _build_mock_ssm_client({secret_name: secret_value})
+    output_file_path = "/test.txt"
+
+    secret_manager = SsmSecretManager(mock_ssm_client)
+
+    secret_manager.download_secret(secret_name, output_file_path)
+
+    actual_secret_value = _read_file(output_file_path)
 
     assert actual_secret_value == secret_value
     mock_ssm_client.get_parameter.assert_called_once_with(Name=secret_name, WithDecryption=True)
