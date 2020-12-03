1e0671901268ce9a2bda49b90e49f428ca97ff95
James Green <jagreen@thoughtworks.com>
2020-12-03 10:13:40 +0000

    PMRT-1349 [DS/JG] Removed command line entrypoint

--- setup.py
+++ setup.py
@@ -6,9 +6,4 @@ setup(
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=["boto3~=1.16", "mesh_client~=0.11"],
-    entry_points={
-        "console_scripts": [
-            "forward-mesh-inbox-to-s3=s3mesh.main:main",
-        ]
-    },
 )
--- src/s3mesh/main.py
+++ src/s3mesh/main.py
@@ -1,5 +1,3 @@
-import sys
-from argparse import ArgumentParser
 from dataclasses import dataclass
 from typing import Optional
 
@@ -10,53 +8,6 @@ from s3mesh.mesh import MeshInbox
 from s3mesh.s3 import S3Uploader
 from s3mesh.service import MeshToS3ForwarderService
 import mesh_client
-import logging
-
-LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-
-
-def main():
-    logging.basicConfig(stream=sys.stdout, level=logging.INFO, format=LOG_FORMAT)
-    args = parse_arguments(sys.argv[1:])
-
-    forwarder_service = build_forwarder_service(
-        mesh_config=MeshConfig(
-            url=args.mesh_url,
-            mailbox=args.mesh_mailbox,
-            password=args.mesh_password,
-            shared_key=args.mesh_shared_key,
-            client_cert_path=args.mesh_client_cert_path,
-            client_key_path=args.mesh_client_key_path,
-            ca_cert_path=args.mesh_ca_cert_path,
-        ),
-        s3_config=S3Config(
-            bucket_name=args.s3_bucket_name,
-            endpoint_url=args.s3_endpoint_url,
-        ),
-        poll_frequency_sec=args.poll_frequency,
-    )
-
-    forwarder_service.start()
-
-
-def utf8_bytes(value):
-    return bytes(value, "utf-8")
-
-
-def parse_arguments(argument_list):
-    parser = ArgumentParser(description="MESH to S3 Forwarder")
-    parser.add_argument("--mesh-mailbox", type=str, required=True)
-    parser.add_argument("--mesh-password", type=str, required=True)
-    parser.add_argument("--mesh-url", type=str, required=True)
-    parser.add_argument("--mesh-shared-key", type=utf8_bytes, required=True)
-    parser.add_argument("--mesh-client-cert-path", type=str, required=True)
-    parser.add_argument("--mesh-client-key-path", type=str, required=True)
-    parser.add_argument("--mesh-ca-cert-path", type=str, required=True)
-    parser.add_argument("--s3-bucket-name", type=str, required=True)
-    parser.add_argument("--s3-endpoint-url", type=str, required=False, default=None)
-    parser.add_argument("--poll-frequency", type=int, required=False, default=10)
-
-    return parser.parse_args(argument_list)
 
 
 @dataclass
