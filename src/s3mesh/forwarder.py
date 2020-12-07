3ed2d7e30d889d4cc900e7d715dd33b1ca1205d5
Helen Zhou <helen.zhou1@nhs.net>
2020-12-07 14:17:56 +0000

    PRMT-1349 [HZ/JG] Introduce meaningful name for exit Event

--- src/s3mesh/forwarder.py
+++ src/s3mesh/forwarder.py
@@ -30,19 +30,19 @@ class MeshToS3Forwarder:
 class MeshToS3ForwarderService:
     def __init__(self, forwarder: MeshToS3Forwarder, poll_frequency_sec: int):
         self._forwarder = forwarder
-        self._exit = Event()
+        self._exit_requested = Event()
         self._poll_frequency_sec = poll_frequency_sec
 
     def start(self):
         logger.info("Started forwarder service")
-        while not self._exit.is_set():
+        while not self._exit_requested.is_set():
             self._forwarder.forward_messages()
-            self._exit.wait(self._poll_frequency_sec)
+            self._exit_requested.wait(self._poll_frequency_sec)
         logger.info("Exiting forwarder service")
 
     def stop(self):
         logger.info("Received request to stop")
-        self._exit.set()
+        self._exit_requested.set()
 
 
 @dataclass
