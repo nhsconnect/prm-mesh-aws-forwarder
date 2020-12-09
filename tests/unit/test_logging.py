f23a2183aea6b1fad1f4c80bbd4b28f378aaba60
Helen Zhou <helen.zhou1@nhs.net>
2020-12-09 16:50:41 +0000

    PMRT-1349 [JG/HZ] Removes unncessary imports

--- tests/unit/test_logging.py
+++ tests/unit/test_logging.py
@@ -1,11 +1,7 @@
-import logging
-import json
+from s3mesh.logging import StructuredMessage, json_log
 
-from unittest import mock
-
-from s3mesh.logging import json_log, StructuredMessage
 
 def test_log_calls_logging_with_json():
-     values = dict(foo='bar', bar='baz', num=123, fnum=123.456)
-     message = json_log(**values)
-     assert message == StructuredMessage(**values)
+    values = {"foo": "bar", "bar": "baz", "num": 123, "fnum": 123.456}
+    message = json_log(**values)
+    assert message == StructuredMessage(**values)
