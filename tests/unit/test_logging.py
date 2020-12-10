01563929d75602f1f9bf4ee4b57a705bdb624b95
Helen Zhou <helen.zhou1@nhs.net>
2020-12-10 10:21:09 +0000

    PMRT-1349 [JG/HZ] Remove Structure Logging

--- tests/unit/test_logging.py
+++
@@ -1,7 +0,0 @@
-from s3mesh.logging import StructuredMessage, json_log
-
-
-def test_log_calls_logging_with_json():
-    values = {"foo": "bar", "bar": "baz", "num": 123, "fnum": 123.456}
-    message = json_log(**values)
-    assert message == StructuredMessage(**values)
