a69ce90a70401c4a4a1742d8c781c509fd60dc40
James Green <jagreen@thoughtworks.com>
2020-12-15 10:53:35 +0000

    PMRT-1463 [JG/MP] Support extra fields in JSON formatted logs

--- src/s3mesh/logging.py
+++ src/s3mesh/logging.py
@@ -1,6 +1,8 @@
 import json
 from datetime import datetime
-from logging import LogRecord
+from logging import LogRecord, makeLogRecord
+
+DEFAULT_LOG_RECORD_ATTRS = vars(makeLogRecord({})).keys()
 
 
 def _convert_timestamp_to_iso(timestamp: float) -> str:
@@ -15,5 +17,10 @@ class JsonFormatter:
             "message": record.msg,
             "time": _convert_timestamp_to_iso(record.created),
         }
+        extra = {
+            attr: getattr(record, attr)
+            for attr in vars(record)
+            if attr not in DEFAULT_LOG_RECORD_ATTRS
+        }
 
-        return json.dumps(base)
+        return json.dumps({**base, **extra})
--- tests/unit/test_logging.py
+++ tests/unit/test_logging.py
@@ -1,29 +1,32 @@
 import json
+from logging import makeLogRecord
 
 import pytest
-from mock import MagicMock
 
 from s3mesh.logging import JsonFormatter
 from tests.builders.common import a_string
 from tests.builders.mesh import an_epoch_timestamp
 
 
-def build_log_record(**kwargs):
-    record = MagicMock()
-    record.levelname = kwargs.get("levelname", a_string())
-    record.module = kwargs.get("module", a_string())
-    record.msg = kwargs.get("msg", a_string())
-    record.created = kwargs.get("created", an_epoch_timestamp())
-    return record
+def _build_log_record(**kwargs):
+    record_fields = {
+        "levelname": kwargs.get("levelname", a_string()),
+        "module": kwargs.get("module", a_string()),
+        "msg": kwargs.get("msg", a_string()),
+        "created": kwargs.get("created", an_epoch_timestamp()),
+    }
+    if "extra" in kwargs:
+        record_fields.update(kwargs.get("extra"))
+    return makeLogRecord(record_fields)
 
 
 @pytest.mark.parametrize(
     ("json_field", "base_attribute"),
     [("level", "levelname"), ("module", "module"), ("message", "msg")],
 )
-def test_base_attributes_are_included_in_json(json_field, base_attribute):
+def test_desired_base_attributes_are_included_in_json(json_field, base_attribute):
     value = a_string()
-    record = build_log_record(**{base_attribute: value})
+    record = _build_log_record(**{base_attribute: value})
 
     actual_json_string = JsonFormatter().format(record)
     actual = json.loads(actual_json_string)[json_field]
@@ -31,11 +34,67 @@ def test_base_attributes_are_included_in_json(json_field, base_attribute):
     assert value == actual
 
 
+@pytest.mark.parametrize(
+    "base_attribute",
+    [
+        "name",
+        "args",
+        "levelno",
+        "pathname",
+        "filename",
+        "exc_info",
+        "exc_text",
+        "stack_info",
+        "lineno",
+        "funcName",
+        "msecs",
+        "relativeCreated",
+        "thread",
+        "threadName",
+        "processName",
+        "process",
+    ],
+)
+def test_undesired_desired_base_attributes_are_not_in_json(base_attribute):
+    record = _build_log_record()
+
+    actual_json_string = JsonFormatter().format(record)
+    actual = json.loads(actual_json_string)
+
+    assert base_attribute not in actual
+
+
 def test_timestamp_is_included_in_json():
-    record = build_log_record(created=1607965513.358049)
+    record = _build_log_record(created=1607965513.358049)
     actual_json_string = JsonFormatter().format(record)
     actual = json.loads(actual_json_string)["time"]
 
     expected = "2020-12-14T17:05:13.358049"
 
     assert actual == expected
+
+
+def test_extra_field_is_included_in_json():
+    record = _build_log_record(extra={"fruit": "mango"})
+
+    actual_json_string = JsonFormatter().format(record)
+    actual = json.loads(actual_json_string)["fruit"]
+
+    expected = "mango"
+
+    assert actual == expected
+
+
+def test_extra_fields_is_included_in_json():
+    record = _build_log_record(extra={"fruit": "mango", "colour": "red"})
+
+    actual_json_string = JsonFormatter().format(record)
+    actual_json = json.loads(actual_json_string)
+
+    actual_fruit = actual_json["fruit"]
+    actual_colour = actual_json["colour"]
+
+    expected_fruit = "mango"
+    expected_colour = "red"
+
+    assert actual_fruit == expected_fruit and actual_colour == expected_colour
