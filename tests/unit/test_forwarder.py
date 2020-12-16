9dfb51efe76d442f8c5002ffb261208c374ed355
James Green <jagreen@thoughtworks.com>
2020-12-16 15:57:31 +0000

    PRMT-1463 [JG/MP] Refactor forwarder tests

--- tests/unit/test_forwarder.py
+++ tests/unit/test_forwarder.py
@@ -23,86 +23,96 @@ def _a_missing_header_exception(**kwargs):
     )
 
 
+def _a_mock_message(**kwargs):
+    message = MagicMock()
+    message.id = kwargs.get("message_id", a_string())
+    message.file_name = kwargs.get("file_name", a_string())
+    message.validate.side_effect = kwargs.get("validation_error", None)
+    return message
+
+
+def _build_forwarder(**kwargs):
+    mock_mesh_inbox = kwargs.get("mesh_inbox", MagicMock())
+    mock_s3_uploader = kwargs.get("s3_uploader", MagicMock())
+    mock_mesh_inbox.read_messages.return_value = iter(kwargs.get("incoming_messages", []))
+
+    return MeshToS3Forwarder(mock_mesh_inbox, mock_s3_uploader)
+
+
 def test_validates_message():
-    mock_mesh_inbox = MagicMock()
-    mock_s3_uploader = MagicMock()
-    mock_mesh_message = MagicMock()
-    mock_mesh_inbox.read_messages.return_value = iter([mock_mesh_message])
-    forwarder = MeshToS3Forwarder(mock_mesh_inbox, mock_s3_uploader)
+    mock_message = _a_mock_message()
+
+    forwarder = _build_forwarder(
+        incoming_messages=[mock_message],
+    )
 
     forwarder.forward_messages()
 
-    mock_mesh_message.validate.assert_called_once()
+    mock_message.validate.assert_called_once()
 
 
 def test_forwards_message():
-    mock_mesh_inbox = MagicMock()
-    mock_s3_uploader = MagicMock()
-    mock_mesh_message = MagicMock()
-    mock_mesh_inbox.read_messages.return_value = iter([mock_mesh_message])
-    forwarder = MeshToS3Forwarder(mock_mesh_inbox, mock_s3_uploader)
+    mock_message = _a_mock_message()
+    mock_uploader = MagicMock()
+
+    forwarder = _build_forwarder(incoming_messages=[mock_message], s3_uploader=mock_uploader)
 
     forwarder.forward_messages()
 
-    mock_s3_uploader.upload.assert_called_once_with(mock_mesh_message)
+    mock_uploader.upload.assert_called_once_with(mock_message)
 
 
 def test_acknowledges_message():
-    mock_mesh_inbox = MagicMock()
-    mock_s3_uploader = MagicMock()
-    mock_mesh_message = MagicMock()
-    mock_mesh_inbox.read_messages.return_value = iter([mock_mesh_message])
-    forwarder = MeshToS3Forwarder(mock_mesh_inbox, mock_s3_uploader)
+    mock_message = _a_mock_message()
+
+    forwarder = _build_forwarder(
+        incoming_messages=[mock_message],
+    )
 
     forwarder.forward_messages()
 
-    mock_mesh_message.acknowledge.assert_called_once()
+    mock_message.acknowledge.assert_called_once()
 
 
 def test_forwards_multiple_messages():
-    mock_mesh_inbox = MagicMock()
-    mock_s3_uploader = MagicMock()
-    mock_mesh_message_one = MagicMock()
-    mock_mesh_message_two = MagicMock()
-    mock_mesh_messages = [mock_mesh_message_one, mock_mesh_message_two]
-    mock_mesh_inbox.read_messages.return_value = iter(mock_mesh_messages)
-    forwarder = MeshToS3Forwarder(mock_mesh_inbox, mock_s3_uploader)
+    mock_message_one = _a_mock_message()
+    mock_message_two = _a_mock_message()
+    mock_uploader = MagicMock()
+
+    forwarder = _build_forwarder(
+        incoming_messages=[mock_message_one, mock_message_two], s3_uploader=mock_uploader
+    )
 
     forwarder.forward_messages()
 
-    mock_s3_uploader.upload.assert_has_calls(
+    mock_uploader.upload.assert_has_calls(
         [
-            call(mock_mesh_message_one),
-            call(mock_mesh_message_two),
+            call(mock_message_one),
+            call(mock_message_two),
         ]
     )
 
-    assert mock_s3_uploader.upload.call_count == 2
+    assert mock_uploader.upload.call_count == 2
 
 
 def test_acknowledges_multiple_message():
-    mock_mesh_inbox = MagicMock()
-    mock_s3_uploader = MagicMock()
-    mock_mesh_messages = [MagicMock(), MagicMock()]
-    mock_mesh_inbox.read_messages.return_value = iter(mock_mesh_messages)
-    forwarder = MeshToS3Forwarder(mock_mesh_inbox, mock_s3_uploader)
+
+    mock_messages = [_a_mock_message(), _a_mock_message()]
+
+    forwarder = _build_forwarder(incoming_messages=mock_messages)
 
     forwarder.forward_messages()
 
-    for mock_mesh_message in mock_mesh_messages:
-        mock_mesh_message.acknowledge.assert_called_once()
+    for mock_message in mock_messages:
+        mock_message.acknowledge.assert_called_once()
 
 
 def test_logs_message_progress():
     logger = logging.getLogger("s3mesh.forwarder")
 
-    mock_mesh_inbox = MagicMock()
-    mock_s3_uploader = MagicMock()
-    mock_mesh_message = MagicMock()
-    mock_mesh_message.id = "123"
-    mock_mesh_message.file_name = "a_file.dat"
-    mock_mesh_inbox.read_messages.return_value = iter([mock_mesh_message])
-    forwarder = MeshToS3Forwarder(mock_mesh_inbox, mock_s3_uploader)
+    forwarder = _build_forwarder(
+        incoming_messages=[_a_mock_message(message_id="123", file_name="a_file.dat")]
+    )
 
     with patch.object(logger, "info") as mock_info:
         forwarder.forward_messages()
@@ -118,12 +128,9 @@ def test_logs_message_progress():
 
 
 def test_catches_invalid_header_error():
-    mock_mesh_inbox = MagicMock()
-    mock_s3_uploader = MagicMock()
-    mock_mesh_message = MagicMock()
-    mock_mesh_inbox.read_messages.return_value = iter([mock_mesh_message])
-    forwarder = MeshToS3Forwarder(mock_mesh_inbox, mock_s3_uploader)
-    mock_mesh_message.validate.side_effect = _an_invalid_header_exception()
+    forwarder = _build_forwarder(
+        incoming_messages=[_a_mock_message(validation_error=_an_invalid_header_exception())]
+    )
 
     try:
         forwarder.forward_messages()
@@ -132,45 +139,44 @@ def test_catches_invalid_header_error():
 
 
 def test_calls_logger_with_a_warning_when_message_has_invalid_header():
-    mock_mesh_inbox = MagicMock()
-    mock_s3_uploader = MagicMock()
-    mock_mesh_message = MagicMock()
-    mock_mesh_message.id = "abc"
-
-    mock_mesh_inbox.read_messages.return_value = iter([mock_mesh_message])
-    forwarder = MeshToS3Forwarder(mock_mesh_inbox, mock_s3_uploader)
-    mock_mesh_message.validate.side_effect = _an_invalid_header_exception(
-        header_name="fruit_header",
-        header_value="banana",
-        expected_header_value="mango",
+    forwarder = _build_forwarder(
+        incoming_messages=[
+            _a_mock_message(
+                message_id="abc",
+                validation_error=_an_invalid_header_exception(
+                    header_name="fruit_header",
+                    header_value="banana",
+                    expected_header_value="mango",
+                ),
+            )
+        ]
     )
+
     logger = logging.getLogger("s3mesh.forwarder")
 
     with mock.patch.object(logger, "warning") as mock_warn:
         forwarder.forward_messages()
 
     mock_warn.assert_called_with(
-        "Message abc: " "Invalid MESH fruit_header header - expected: mango, " "instead got: banana"
+        "Message abc: Invalid MESH fruit_header header - expected: mango, instead got: banana"
     )
 
 
 def test_continues_uploading_messages_when_one_of_them_has_invalid_mesh_header():
-    mock_mesh_inbox = MagicMock()
-    mock_s3_uploader = MagicMock()
-    successful_message_1 = MagicMock()
-    successful_message_2 = MagicMock()
-    unsuccessful_message = MagicMock()
-    unsuccessful_message.validate.side_effect = _an_invalid_header_exception()
+    successful_message_1 = _a_mock_message()
+    successful_message_2 = _a_mock_message()
+    unsuccessful_message = _a_mock_message(validation_error=_an_invalid_header_exception())
 
-    mock_mesh_inbox.read_messages.return_value = iter(
-        [successful_message_1, unsuccessful_message, successful_message_2]
+    mock_uploader = MagicMock()
+
+    forwarder = _build_forwarder(
+        incoming_messages=[successful_message_1, unsuccessful_message, successful_message_2],
+        s3_uploader=mock_uploader,
     )
-
-    forwarder = MeshToS3Forwarder(mock_mesh_inbox, mock_s3_uploader)
 
     forwarder.forward_messages()
 
-    mock_s3_uploader.upload.assert_has_calls(
+    mock_uploader.upload.assert_has_calls(
         [
             call(successful_message_1),
             call(successful_message_2),
@@ -179,12 +185,9 @@ def test_continues_uploading_messages_when_one_of_them_has_invalid_mesh_header()
 
 
 def test_catches_missing_header_error():
-    mock_mesh_inbox = MagicMock()
-    mock_s3_uploader = MagicMock()
-    mock_mesh_message = MagicMock()
-    mock_mesh_inbox.read_messages.return_value = iter([mock_mesh_message])
-    forwarder = MeshToS3Forwarder(mock_mesh_inbox, mock_s3_uploader)
-    mock_mesh_message.validate.side_effect = _a_missing_header_exception()
+    forwarder = _build_forwarder(
+        incoming_messages=[_a_mock_message(validation_error=_a_missing_header_exception())]
+    )
 
     try:
         forwarder.forward_messages()
@@ -193,16 +196,17 @@ def test_catches_missing_header_error():
 
 
 def test_calls_logger_with_a_warning_when_message_is_missing_header():
-    mock_mesh_inbox = MagicMock()
-    mock_s3_uploader = MagicMock()
-    mock_mesh_message = MagicMock()
-    mock_mesh_message.id = "abc"
-
-    mock_mesh_inbox.read_messages.return_value = iter([mock_mesh_message])
-    forwarder = MeshToS3Forwarder(mock_mesh_inbox, mock_s3_uploader)
-    mock_mesh_message.validate.side_effect = _a_missing_header_exception(
-        header_name="fruit_header",
+    forwarder = _build_forwarder(
+        incoming_messages=[
+            _a_mock_message(
+                message_id="abc",
+                validation_error=_a_missing_header_exception(
+                    header_name="fruit_header",
+                ),
+            )
+        ]
     )
+
     logger = logging.getLogger("s3mesh.forwarder")
 
     with mock.patch.object(logger, "warning") as mock_warn:
@@ -212,22 +216,20 @@ def test_calls_logger_with_a_warning_when_message_is_missing_header():
 
 
 def test_continues_uploading_messages_when_one_of_them_has_missing_mesh_header():
-    mock_mesh_inbox = MagicMock()
-    mock_s3_uploader = MagicMock()
-    successful_message_1 = MagicMock()
-    successful_message_2 = MagicMock()
-    unsuccessful_message = MagicMock()
-    unsuccessful_message.validate.side_effect = _a_missing_header_exception()
+    successful_message_1 = _a_mock_message()
+    successful_message_2 = _a_mock_message()
+    unsuccessful_message = _a_mock_message(validation_error=_a_missing_header_exception())
 
-    mock_mesh_inbox.read_messages.return_value = iter(
-        [successful_message_1, unsuccessful_message, successful_message_2]
+    mock_uploader = MagicMock()
+
+    forwarder = _build_forwarder(
+        incoming_messages=[successful_message_1, unsuccessful_message, successful_message_2],
+        s3_uploader=mock_uploader,
     )
-
-    forwarder = MeshToS3Forwarder(mock_mesh_inbox, mock_s3_uploader)
 
     forwarder.forward_messages()
 
-    mock_s3_uploader.upload.assert_has_calls(
+    mock_uploader.upload.assert_has_calls(
         [
             call(successful_message_1),
             call(successful_message_2),
@@ -236,12 +238,7 @@ def test_continues_uploading_messages_when_one_of_them_has_missing_mesh_header()
 
 
 def test_does_not_catch_generic_exception():
-    mock_mesh_inbox = MagicMock()
-    mock_s3_uploader = MagicMock()
-    mock_mesh_message = MagicMock()
-    mock_mesh_inbox.read_messages.return_value = iter([mock_mesh_message])
-    forwarder = MeshToS3Forwarder(mock_mesh_inbox, mock_s3_uploader)
-    mock_mesh_message.validate.side_effect = Exception
+    forwarder = _build_forwarder(incoming_messages=[_a_mock_message(validation_error=Exception)])
 
     with pytest.raises(Exception):
         forwarder.forward_messages()
