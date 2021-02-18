08a7f7f2f7c8e8bfc43fead34010b00655254c5a
James Green <jagreen@thoughtworks.com>
2021-02-18 17:32:01 +0000

    PRMT-TECH Ensure forward message event records invalid mesh header

--- src/s3mesh/monitoring/event/forward.py
+++ src/s3mesh/monitoring/event/forward.py
@@ -1,5 +1,5 @@
-from s3mesh.forwarder import MISSING_MESH_HEADER_ERROR
-from s3mesh.mesh import MeshMessage, MissingMeshHeader
+from s3mesh.forwarder import INVALID_MESH_HEADER_ERROR, MISSING_MESH_HEADER_ERROR
+from s3mesh.mesh import InvalidMeshHeader, MeshMessage, MissingMeshHeader
 
 FORWARD_MESSAGE_EVENT = "FORWARD_MESH_MESSAGE"
 
@@ -21,6 +21,12 @@ class ForwardMessageEvent:
     def record_missing_mesh_header(self, exception: MissingMeshHeader):
         self._fields["error"] = MISSING_MESH_HEADER_ERROR
         self._fields["missingHeaderName"] = exception.header_name
+
+    def record_invalid_mesh_header(self, exception: InvalidMeshHeader):
+        self._fields["error"] = INVALID_MESH_HEADER_ERROR
+        self._fields["headerName"] = exception.header_name
+        self._fields["expectedHeaderValue"] = exception.expected_header_value
+        self._fields["receivedHeaderValue"] = exception.header_value
 
     def finish(self):
         self._output.log_event(FORWARD_MESSAGE_EVENT, self._fields)
--- tests/unit/test_monitoring_event_forward.py
+++ tests/unit/test_monitoring_event_forward.py
@@ -1,7 +1,7 @@
 from mock import MagicMock
 
-from s3mesh.forwarder import MISSING_MESH_HEADER_ERROR
-from s3mesh.mesh import MissingMeshHeader
+from s3mesh.forwarder import INVALID_MESH_HEADER_ERROR, MISSING_MESH_HEADER_ERROR
+from s3mesh.mesh import InvalidMeshHeader, MissingMeshHeader
 from s3mesh.monitoring.event.forward import FORWARD_MESSAGE_EVENT, ForwardMessageEvent
 from tests.builders.common import a_string
 from tests.builders.mesh import mock_mesh_message
@@ -59,5 +59,26 @@ def test_record_missing_mesh_header():
         {
             "error": MISSING_MESH_HEADER_ERROR,
             "missingHeaderName": missing_header_exception.header_name,
+        },
+    )
+
+
+def test_record_invalid_mesh_header():
+    mock_output = MagicMock()
+    invalid_header_exception = InvalidMeshHeader(
+        header_name=a_string(), header_value=a_string(), expected_header_value=a_string()
+    )
+
+    forward_message_event = ForwardMessageEvent(mock_output)
+    forward_message_event.record_invalid_mesh_header(invalid_header_exception)
+    forward_message_event.finish()
+
+    mock_output.log_event.assert_called_with(
+        FORWARD_MESSAGE_EVENT,
+        {
+            "error": INVALID_MESH_HEADER_ERROR,
+            "headerName": invalid_header_exception.header_name,
+            "expectedHeaderValue": invalid_header_exception.expected_header_value,
+            "receivedHeaderValue": invalid_header_exception.header_value,
         },
     )
