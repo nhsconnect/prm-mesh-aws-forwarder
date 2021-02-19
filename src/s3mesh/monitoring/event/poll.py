e5e7aa89550eaecccbeb6f91122e0e4958ff0995
James Green <jagreen@thoughtworks.com>
2021-02-19 16:40:21 +0000

    PRMT-TECH Record message batch size in poll inbox event

--- src/s3mesh/monitoring/event/poll.py
+++ src/s3mesh/monitoring/event/poll.py
@@ -6,5 +6,8 @@ class PollInboxEvent:
         self._fields = {}
         self._output = output
 
+    def record_message_batch_count(self, count: int):
+        self._fields["batchMessageCount"] = count
+
     def finish(self):
         self._output.log_event(POLL_INBOX_EVENT, self._fields)
--- tests/unit/test_monitoring_event_poll.py
+++ tests/unit/test_monitoring_event_poll.py
@@ -6,7 +6,17 @@ from s3mesh.monitoring.event.poll import POLL_INBOX_EVENT, PollInboxEvent
 def test_finish_calls_log_event_with_event_name():
     mock_output = MagicMock()
 
-    forward_message_event = PollInboxEvent(mock_output)
-    forward_message_event.finish()
+    poll_inbox_event = PollInboxEvent(mock_output)
+    poll_inbox_event.finish()
 
     mock_output.log_event.assert_called_with(POLL_INBOX_EVENT, {})
+
+
+def test_record_message_batch_count():
+    mock_output = MagicMock()
+
+    poll_inbox_event = PollInboxEvent(mock_output)
+    poll_inbox_event.record_message_batch_count(2)
+    poll_inbox_event.finish()
+
+    mock_output.log_event.assert_called_with(POLL_INBOX_EVENT, {"batchMessageCount": 2})
