71d78680841dbbd9edd1f4ae7ed52b44d4691a67
James Green <jagreen@thoughtworks.com>
2021-02-19 16:41:55 +0000

    PRMT-TECH Bring test names in line with existing ones

--- tests/unit/test_monitoring_event_count.py
+++ tests/unit/test_monitoring_event_count.py
@@ -13,7 +13,7 @@ def test_finish_calls_log_event_with_event_name():
     mock_output.log_event.assert_called_with(COUNT_MESSAGES_EVENT, {})
 
 
-def test_record_message_count_records_inbox_message_count():
+def test_record_message_count():
     mock_output = MagicMock()
     message_count = 2
 
@@ -26,7 +26,7 @@ def test_record_message_count_records_inbox_message_count():
     )
 
 
-def test_record_mesh_client_network_error_records_error_details():
+def test_record_mesh_client_network_error():
     mock_output = MagicMock()
     error_message = "Oh no!"
     mock_exception = MagicMock()
