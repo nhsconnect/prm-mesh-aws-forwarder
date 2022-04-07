6a92d96dea5d635766caba26f4209e6d6e2c3188
Dan Moore <dan.moore1@nhs.net>
2022-04-07 10:54:14 +0100

    [PRMT-2594] Moving a bit of noise out of sns e2e test

--- tests/e2e/test_e2e_sns_forwarder.py
+++ tests/e2e/test_e2e_sns_forwarder.py
@@ -35,6 +35,60 @@ def _build_sns_forwarder(config, sns_topic_arn):
     return ThreadedForwarder(forwarder)
 
 
+class SnsTopic:
+    def __init__(self, aws_client, arn) -> None:
+        self.arn = arn
+        self._aws_client = aws_client
+
+    def subscribe(self, queue):
+        self._aws_client.subscribe(
+            TopicArn=self.arn,
+            Protocol="sqs",
+            Endpoint=queue.arn,
+            Attributes={"RawMessageDelivery": "true"},
+        )
+
+
+class SnsForTesting:
+    def __init__(self, e2e_test_context) -> None:
+        self._aws_client = e2e_test_context.build_aws_client("sns")
+
+    def create_topic(self, sns_topic_name):
+        self._aws_client.create_topic(Name=sns_topic_name)
+        topic_list_response = self._aws_client.list_topics()
+        topic_arn = topic_list_response["Topics"][0]["TopicArn"]
+        return SnsTopic(self._aws_client, topic_arn)
+
+
+class SqsQueue:
+    def __init__(self, aws_client, name, url, arn) -> None:
+        self.name = name
+        self.url = url
+        self.arn = arn
+        self._aws_client = aws_client
+
+    def receive_messages(self):
+        messages = self._aws_client.receive_message(
+            QueueUrl=self.url, WaitTimeSeconds=10, MessageAttributeNames=["All"]
+        )["Messages"]
+        return messages
+
+
+class SqsForTesting:
+    def __init__(self, e2e_test_context) -> None:
+        self._aws_client = e2e_test_context.build_aws_client("sqs")
+        self.sqs_arn = e2e_test_context.get_service_arn("sqs")
+
+    @property
+    def aws_client(self):
+        return self._aws_client
+
+    def create_queue(self, queue_name):
+        queue_url = self._aws_client.create_queue(QueueName=queue_name)["QueueUrl"]
+        queue_arn = f"{self.sqs_arn}:{queue_name}"
+        return SqsQueue(self._aws_client, queue_name, queue_url, queue_arn)
+
+
 def test_mesh_inbox_sns_forwarder(e2e_test_context):
     mesh = e2e_test_context.build_mesh_client(SENDING_MESH_MAILBOX)
 
@@ -42,39 +96,29 @@ def test_mesh_inbox_sns_forwarder(e2e_test_context):
     e2e_test_context.populate_ssm_parameters(
         test_name="test-sns", receiving_mailbox_name=RECEIVING_MESH_MAILBOX
     )
-    sns = e2e_test_context.build_aws_client("sns")
-    sqs = e2e_test_context.build_aws_client("sqs")
+    sns = SnsForTesting(e2e_test_context)
+    sqs = SqsForTesting(e2e_test_context)
 
-    sns.create_topic(Name="test-topic")
-    queue_name = "test-queue"
-    queue_url = sqs.create_queue(QueueName=queue_name)["QueueUrl"]
-    topic_list_response = sns.list_topics()
-    topic_arn = topic_list_response["Topics"][0]["TopicArn"]
-    sqs_arn = e2e_test_context.get_service_arn("sqs")
-    sns.subscribe(
-        TopicArn=topic_arn,
-        Protocol="sqs",
-        Endpoint=f"{sqs_arn}:{queue_name}",
-        Attributes={"RawMessageDelivery": "true"},
-    )
+    queue = sqs.create_queue("test-queue")
+    topic = sns.create_topic("test-topic")
+
+    topic.subscribe(queue)
 
     message_contents = a_string()
 
     forwarder = _build_sns_forwarder(
-        sns_topic_arn=topic_arn,
+        sns_topic_arn=topic.arn,
         config=e2e_test_context.build_forwarder_config(test_name="test-sns"),
     )
+
     try:
         forwarder.start()
         mesh.send_message(RECEIVING_MESH_MAILBOX, message_contents.encode("utf-8"))
-        messages = sqs.receive_message(
-            QueueUrl=queue_url, WaitTimeSeconds=10, MessageAttributeNames=["All"]
-        )["Messages"]
-        message_body = messages[0]["Body"]
-        message_attributes = messages[0]["MessageAttributes"]
 
-        assert message_body == message_contents
-        assert "meshMessageId" in message_attributes
+        message = queue.receive_messages()[0]
+
+        assert message["Body"] == message_contents
+        assert "meshMessageId" in message["MessageAttributes"]
     finally:
         forwarder.stop()
         e2e_test_context.unset_fake_aws_environment_vars()
