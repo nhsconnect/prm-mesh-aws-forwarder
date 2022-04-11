e6214b3b751f131057e28269fd5abb491ca3fd2a
Mario Giampietri <mario.giampietri1@nhs.net>
2022-04-11 17:09:00 +0100

    [PRMT-2594] added logging for message exception(s) detected while raising a single exception

--- tests/unit/test_forwarder.py
+++ tests/unit/test_forwarder.py
@@ -1,6 +1,7 @@
 from unittest import mock
-from unittest.mock import MagicMock, call
+from unittest.mock import MagicMock, call, patch
 
+import logging
 import pytest
 
 from awsmesh.forwarder import RetryableException
@@ -338,6 +339,26 @@ def test_that_when_a_network_error_occurs_on_retrieving_a_message_it_continues_t
         pass
 
     mock_uploader.upload.assert_called_once_with(good_message, mock.ANY)
+
+def test_that_when_a_network_error_occurs_on_retrieving_multiple_messages_it_logs_them():
+    mock_uploader = MagicMock()
+    logger = logging.getLogger("awsmesh.forwarder")
+
+    good_message = mock_mesh_message()
+    forwarder = build_forwarder(
+        list_message_ids=["bad_message_id", good_message.id, "another_bad_message_id"],
+        retrieve_message=[mesh_client_network_error(), good_message, mesh_client_network_error()],
+        uploader=mock_uploader,
+    )
+
+    try:
+        with patch.object(logger, "info") as mock_info:
+            forwarder.forward_messages()
+    except Exception:
+        pass
+
+    mock_uploader.upload.assert_called_once_with(good_message, mock.ANY)
+    mock_info.assert_called_once_with("Raising single retryable exception, actually caught 2 message exception(s)")
 
 
 # flake8: noqa: E501
